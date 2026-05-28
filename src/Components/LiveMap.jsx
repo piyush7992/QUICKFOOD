7132c72b4e9c5463de60ff87cfd023ec9c08535f
piyush7992 <piyushsingh08208@gmail.com>
2026-05-28 15:11:32 +0530

    com

--- src/Components/LiveMap.jsx
+++ src/Components/LiveMap.jsx
@@ -1,63 +1,63 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-import { useEffect, useState } from "react";
+// import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+// import { useEffect, useState } from "react";
 
-// THIS WILL MOVE MAP WHEN RIDER MOVES
-function ChangeMapView({ center }) {
-  const map = useMap();
-  map.setView(center);
-  return null;
-}
+// // THIS WILL MOVE MAP WHEN RIDER MOVES
+// function ChangeMapView({ center }) {
+//   const map = useMap();
+//   map.setView(center);
+//   return null;
+// }
 
-export default function LiveMap({ orderId }) {
+// export default function LiveMap({ orderId }) {
 
-  const [riderLocation, setRiderLocation] = useState({
-    lat: 22.5726,
-    lng: 88.3639
-  });
+//   const [riderLocation, setRiderLocation] = useState({
+//     lat: 22.5726,
+//     lng: 88.3639
+//   });
 
-  useEffect(() => {
-socket.emit("join-order-room", orderId);  
-    const handleLocationUpdate = (data) => {
+//   useEffect(() => {
+// socket.emit("join-order-room", orderId);  
+//     const handleLocationUpdate = (data) => {
 
-      if (data.orderId === orderId) {
-        setRiderLocation({
-          lat: data.lat,
-          lng: data.lng
-        });
-      }
+//       if (data.orderId === orderId) {
+//         setRiderLocation({
+//           lat: data.lat,
+//           lng: data.lng
+//         });
+//       }
 
-    };
+//     };
 
-    socket.on("riderLocationUpdate", handleLocationUpdate);
+//     socket.on("riderLocationUpdate", handleLocationUpdate);
 
-    // CLEANUP (IMPORTANT)
-    return () => {
-      socket.off("riderLocationUpdate", handleLocationUpdate);
-    };
+//     // CLEANUP (IMPORTANT)
+//     return () => {
+//       socket.off("riderLocationUpdate", handleLocationUpdate);
+//     };
 
-  }, [orderId]);
+//   }, [orderId]);
 
-  return (
-    <MapContainer
-      center={[riderLocation.lat, riderLocation.lng]}
-      zoom={15}
-      style={{ height: "400px", width: "100%" }}
-    >
+//   return (
+//     <MapContainer
+//       center={[riderLocation.lat, riderLocation.lng]}
+//       zoom={15}
+//       style={{ height: "400px", width: "100%" }}
+//     >
 
-      <ChangeMapView
-        center={[riderLocation.lat, riderLocation.lng]}
-      />
+//       <ChangeMapView
+//         center={[riderLocation.lat, riderLocation.lng]}
+//       />
 
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+//       <TileLayer
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
 
-      <Marker position={[riderLocation.lat, riderLocation.lng]}>
-        <Popup>
-          Rider is coming 🚴
-        </Popup>
-      </Marker>
+//       <Marker position={[riderLocation.lat, riderLocation.lng]}>
+//         <Popup>
+//           Rider is coming 🚴
+//         </Popup>
+//       </Marker>
 
-    </MapContainer>
-  );
-}
+//     </MapContainer>
+//   );
+// }
